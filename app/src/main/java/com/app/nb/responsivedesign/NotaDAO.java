71246c1812212007068bb09761e2ffbe811f69e9
namelessbliss <anonimo2911.2012@gmail.com>
2019-09-23 17:06:55 -0500

    Implementando Interfaz DAO con LiveData

---
+++ app/src/main/java/com/app/nb/responsivedesign/NotaDAO.java
@@ -0,0 +1,32 @@
+package com.app.nb.responsivedesign;
+
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import java.util.List;
+
+@Dao
+public interface NotaDAO {
+
+    @Insert
+    void insertarNota(NotaEntity nota);
+
+    @Update
+    void actualizarNota(NotaEntity nota);
+
+    @Query("DELETE FROM NOTAS")
+    void borrarTodo();
+
+    @Query("DELETE FROM NOTAS WHERE id = :idNota")
+    void borrarNotaPorId(int idNota);
+
+    @Query("SELECT * FROM NOTAS ORDER BY titulo ASC")
+    LiveData<List<NotaEntity>> getNotas();
+
+    @Query("SELECT * FROM NOTAS WHERE FAVORITA LIKE 1")
+    LiveData<List<NotaEntity>> getNotasFavoritas();
+
+}
